ec6438c203b519f42599dd4cf3d8b1560f991b74
Morce Faster <morcefaster@corporations.win>
2021-12-03 11:32:29 +0200

    Local changes

--- components/providers/firebase.js
+++ components/providers/firebase.js
@@ -4,14 +4,13 @@ import "firebase/database";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyBnJN03nshL_iTaokPzIzP-8DxMIgoJ1dQ",
+  authDomain: "afk-nn.firebaseapp.com",
+  databaseURL: "https://afk-nn-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "afk-nn",
+  storageBucket: "afk-nn.appspot.com",
+  messagingSenderId: "522161399276",
+  appId: "1:522161399276:web:6763870764cfbc464bccc9"
 };
 
 if (!firebase.apps.length) {
--- server.js
+++ server.js
@@ -1,7 +1,35 @@
+// const { createServer } = require("https");
+// const { parse } = require("url");
+// const next = require("next");
+// const fs = require("fs");
+
+// process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// const httpsOptions = {
+//   key: fs.readFileSync("./certificates/localhost.key"),
+//   cert: fs.readFileSync("./certificates/localhost.crt"),
+// };
+
+// app.prepare().then(() => {
+//   createServer(httpsOptions, (req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     handle(req, res, parsedUrl);
+//   }).listen(3000, (err) => {
+//     if (err) throw err;
+//     console.log("> Ready on https://localhost:3000");
+//   });
+// });
+
+
 const { createServer } = require("https");
 const { parse } = require("url");
 const next = require("next");
 const fs = require("fs");
+const http = require("http");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
@@ -9,17 +37,13 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: fs.readFileSync("./certificates/localhost.key"),
-  cert: fs.readFileSync("./certificates/localhost.crt"),
-};
 
 app.prepare().then(() => {
-  createServer(httpsOptions, (req, res) => {
+  http.createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, (err) => {
+  }).listen(80, (err) => {
     if (err) throw err;
-    console.log("> Ready on https://localhost:3000");
+    console.log("> Ready on https://localhost:80");
   });
 });
